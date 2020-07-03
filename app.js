de0f5a198ee201b5ddf13fbc55b825c1785b7ccc
Arek <hallmann92@wp.pl>
2020-07-03 07:26:26 +0200

    added state handler and server access layer

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
-const index = require('./routes/changeState');
+const changeState = require('./routes/changeState');
+const sender = require('./serverAccessLayer')
+
+const maxPort = 40000;
+const minPort = 3000;
 
 const app = express();
-const port = 3000;
+const port = process.argv.slice(2)[0] | Math.floor(Math.random() * (maxPort - minPort + 1)) + minPort;
+const id = process.argv.slice(2)[1] | port
+const url = `http://localhost:${port}`;
+const serverUrl = 'http://localhost:2000'; // do configu
 
 // view engine setup
 
@@ -11,8 +18,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', changeState);
 
 app.listen(port, () => {
-  console.log(`Listening to requests on http://localhost:${port}`);
+  console.log(`Listening on http://localhost:${port}`);
 });
+
+const body = {id, url}
+sender.sendData(body, serverUrl)
+    .then(result => console.log(`Response: ${JSON.stringify(result)}`))
+    .catch(err => console.log(`${err}`));
